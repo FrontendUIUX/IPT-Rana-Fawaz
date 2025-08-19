12a47dc3295e9c13ba3bc2ca99bf6ef75d1d7eee
RanaFawaz200416 <165808534+RanaFawaz200416@users.noreply.github.com>
2025-08-19 10:04:44 +0300

    Add files via upload

--- css/Sidebar.js
+++ css/Sidebar.js
@@ -250,10 +250,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-$viewDiv = $("div [name='Area Item OBB_Collapse']"); 
-$gridBody = $viewDiv.find('.grid-body'); 
-$gridTable = $gridBody.find("table"); 
-$gridTable.css("min-width", "130%"); 
-$gridTable.css("overflow-x", "scroll");
-
-$("div[name='Area Item OBB_Collapse']").find("colgroup col").removeAttr("style").end().find("table").css({"table-layout": "auto",});
